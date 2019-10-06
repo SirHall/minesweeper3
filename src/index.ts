7684ddeb282ee37926034de3988403a6af2553f8
SirHall <sirhallstein@gmail.com>
2019-10-06 22:32:08 +1000

    Setup rendering and clicking behaviours from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import * as p5 from "p5";
 import * as Honeycomb from "honeycomb-grid";
 import * as CellBasic from "./CellBasic";
-import * as CellWater from "./CellWater";
+import moment = require("moment");
 
 const cellPixSize = 16;
 const gridCellsPerSide = 32;
 const gridPixSize = gridCellsPerSide * cellPixSize;
 
 //Define the hex cell
-const Hex = Honeycomb.extendHex(CellWater.CreateCellWater());
+const Hex = Honeycomb.extendHex(CellBasic.CreateCell());
 
 const grid = Honeycomb.defineGrid(Hex)// 2a. create a rectangular grid:
 const rect = grid.rectangle({ width: gridCellsPerSide, height: gridCellsPerSide });
 
+//Setup each cell
+rect.forEach((hex, index) => {
+    hex.Assign(rect, index, hex);
+});
+
+
+let lastRender: moment.Moment;
+lastRender = moment();
+let forceRedraw: boolean = true;
 
 const p5Instance = new p5(
     (sketch: p5) => {
@@ -21,24 +30,36 @@ const p5Instance = new p5(
                 gridPixSize,
                 gridPixSize
             );
-            document.addEventListener('click', ({ offsetX, offsetY }) => {
-                // convert point to hex (coordinates)
-                const hexCoordinates = grid.pointToHex(offsetX, offsetY)
-                // get the actual hex from the grid
-                console.log(rect.get(hexCoordinates))
-            })
+            sketch.mousePressed =
+                function () {
+                    forceRedraw = true;
+                    // convert point to hex (coordinates)
+                    let hexCoordinates = grid.pointToHex(sketch.mouseX, sketch.mouseY)
+                    // hexCoordinates.
+                    // get the actual hex from the grid
+                    let clicked = rect.get(hexCoordinates);
+
+                    clicked.Reveal();
+                };
         };
 
         sketch.draw = () => {
-            sketch.background(0);
-            sketch.fill(255);
-            sketch.push();
-            rect.forEach(hex => {
-                hex.Draw();
-                // const point = hex.toPoint();
-                // polygon(point.x, point.y, this.size, 6, sketch.radians(30), sketch);
-            });
-            sketch.pop();
+            // sketch.background("black");
+
+            let now = moment();
+            if ((moment.duration(now.diff(lastRender)) > moment.duration(1000, 'ms')) || forceRedraw) {
+                forceRedraw = false;
+                lastRender = moment();
+
+                sketch.fill("black");
+                sketch.stroke("white");
+                sketch.push();
+                rect.forEach(hex => {
+                    hex.Draw(sketch);
+                });
+                sketch.pop();
+            }
+
         };
     }
 );
